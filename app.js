91e9035e4cffcfaf3966e00ce7dc9b1ed6702f79
Abheekrishna <abheekrishna17@gmail.com>
2023-07-09 14:24:30 +0530

    Added reset btn

---
+++ app.js
@@ -0,0 +1,6 @@
+const timeText = document.getElementById('time');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+
+
